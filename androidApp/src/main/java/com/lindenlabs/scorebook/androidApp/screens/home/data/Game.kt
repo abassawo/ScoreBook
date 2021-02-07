24b786e0327af10f87379383b7e70624cd094a64
Abass Bayo-Awoyemi <abassbayo@Abasss-MBP.lan>
2021-02-07 01:00:30 -0500

    Created app scaffold and defined home screen ui

---
+++ androidApp/src/main/java/com/lindenlabs/scorebook/androidApp/screens/home/data/Game.kt
@@ -0,0 +1,3 @@
+package com.lindenlabs.scorebook.androidApp.screens.home.data
+
+data class Game(val isInSession: Boolean, val players: List<Player>)
---
+++ androidApp/src/main/java/com/lindenlabs/scorebook/androidApp/screens/home/domain/IsFirstRun.kt
@@ -0,0 +1,5 @@
+package com.lindenlabs.scorebook.androidApp.screens.home.domain
+
+object IsFirstRun  {
+    operator fun invoke() = true // todo - get this value from shared preferences
+}
