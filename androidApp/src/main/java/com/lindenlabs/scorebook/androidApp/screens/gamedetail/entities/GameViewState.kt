619c0692c272efce7d44bba2745b3022d77d8c4c
Abass Bayo-Awoyemi <abassbayo@Abasss-MBP.lan>
2021-02-20 08:05:27 -0500

    Add support for starting and ending game

--- androidApp/src/main/java/com/lindenlabs/scorebook/androidApp/screens/gamedetail/entities/GameViewState.kt
+++ androidApp/src/main/java/com/lindenlabs/scorebook/androidApp/screens/gamedetail/entities/GameViewState.kt
@@ -1,6 +1,12 @@
 package com.lindenlabs.scorebook.androidApp.screens.gamedetail.entities
 
 sealed class GameViewState(open val gameName: String) {
+
     data class EmptyState(override val gameName: String) : GameViewState(gameName)
-    data class ActiveGame(val players: List<PlayerEntity>, override val gameName: String) : GameViewState(gameName)
+
+    data class ActiveGame(val players: List<PlayerEntity>, override val gameName: String) :
+        GameViewState(gameName)
+
+    data class GameOver(val result: String, override val gameName: String) :
+        GameViewState(gameName)
 }
