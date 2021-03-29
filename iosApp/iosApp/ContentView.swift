ae586235682069e6af48012f7a0415edd18c8847
abassawo <abass.bayo@n26.com>
2021-03-28 22:42:10 -0400

    SwiftUI work - wip

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -8,14 +8,30 @@ func greet() -> String {
 struct ContentView: View {
     var body: some View {
         return ZStack(content: {
-             RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-             RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-             Text(greet()).foregroundColor(Color.orange)
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text(greet()).foregroundColor(Color.orange)
+            HStack(content: {
+
+            }).padding()
         }).padding()
-          .foregroundColor(Color.orange)
-          .font(Font.largeTitle)
+                    .foregroundColor(Color.orange)
+                    .font(Font.largeTitle)
     }
+
 }
+
+// struct ContentView: View {
+//     var body: some View {
+//         return ZStack(content: {
+//              RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+//              RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+//              Text(greet()).foregroundColor(Color.orange)
+//         }).padding()
+//           .foregroundColor(Color.orange)
+//           .font(Font.largeTitle)
+//     }
+// }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
