9fef3ce875f01854ed3bbcb14678adc7642526be
abassawo <abass.bayo@n26.com>
2021-04-05 01:08:34 -0400

    Fix package structure of sql delight foldes

--- shared/src/androidMain/kotlin/com/lindenlabs/scorebook/shared/common/data/DriverFactory.kt
+++ shared/src/androidMain/kotlin/com/lindenlabs/scorebook/shared/common/data/DriverFactory.kt
@@ -2,6 +2,6 @@ package com.lindenlabs.scorebook.shared.common.data
 
 actual class DriverFactory(val context: android.content.Context)  {
     actual fun createDriver() {
-
+        AppDatabase
     }
 }
