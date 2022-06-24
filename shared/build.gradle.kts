e395119bb3b45bb24c4becd9b85fd233847729ae
Abass Bayo <abass@ootp.io>
2022-06-23 23:31:22 -0400

    wip

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.android.library")
     id("com.squareup.sqldelight")
     kotlin("kapt")
+    kotlin("plugin.serialization") version "1.6.21"
 }
 
 kotlin {
@@ -20,18 +21,17 @@ kotlin {
         }
     }
 
-//    val onPhone = System.getenv("SDK_NAME")?.startsWith("iphoneos") ?: false
-//    if (onPhone) iosArm64("ios")
-//    else iosX64("ios")
+    val onPhone = System.getenv("SDK_NAME")?.startsWith("iphoneos") ?: false
+    if (onPhone) iosArm64("ios")
+    else iosX64("ios")
 
     // or sourceSets.iosMain, sourceSets.windowsMain, etc.
     sourceSets {
         val commonMain by getting {
-//            resources.srcDir("src/commonMain/sqldelight")
+            resources.srcDir("src/commonMain/sqldelight")
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
                 implementation("com.squareup.sqldelight:runtime:1.5.3")
-                implementation("com.squareup.sqldelight:native-driver:1.5.3")
             }
         }
         val androidMain by getting {
@@ -41,6 +41,18 @@ kotlin {
                 implementation("com.google.android.material:material:1.3.0")
             }
         }
+
+        val iosMain by getting {
+            dependsOn(commonMain)
+//            iosX64Main.dependsOn(this)
+//            iosArm64Main.dependsOn(this)
+//            iosSimulatorArm64Main.dependsOn(this)
+
+            dependencies {
+                implementation("com.squareup.sqldelight:native-driver:1.4.4")
+            }
+        }
+
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
@@ -50,33 +62,18 @@ kotlin {
         val androidTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
-//        val iosSimulatorArm64Main by getting
-        val iosSimulatorArm64Main by getting {
-            dependencies {
-//                implementation("com.squareup.sqldelight:native-driver:1.4.4")
-            }
-        }
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-
-//            dependencies {
-//                implementation("com.squareup.sqldelight:native-driver:1.4.4")
-//            }
-        }
+        val iosSimulatorArm64Main by getting
 
         val iosX64Test by getting
         val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
 
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
+//        val iosTest by creating {
+//            dependsOn(commonTest)
+//            iosX64Test.dependsOn(this)
+//            iosArm64Test.dependsOn(this)
+//            iosSimulatorArm64Test.dependsOn(this)
+//        }
     }
 }
 
